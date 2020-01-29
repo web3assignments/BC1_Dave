8bbe052a34fa47c6e6d8340fe0b865d94c86298a
emroelofsen <47346232+emroelofsen@users.noreply.github.com>
2020-01-29 19:26:30 +0100

    v2 call function
    TypeError: ContractTestPay.methods.ContractBalance is not a function

--- Day_04_Wed_27_11_2019/ShowWorkedHours.js
+++ Day_04_Wed_27_11_2019/ShowWorkedHours.js
@@ -1,9 +1,24 @@
 const Web3 = require('web3');
-const web3 = new Web3 ('https://ropsten.infura.io');
-
-await window.ethereum.enable();
-
-const WorkedHours = web3.eth.Contract(contract_abi, contract_address);
-
-WorkedHours.methods.NrofAddress().call();
-
+const web3 = new Web3('https://ropsten.infura.io');
+const TestPayABI = [{
+    "constant": true,
+    "inputs": [],
+    "name": "Balance",
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}];
+async function f() {
+    const TestPayaddress = "0x0060f37cdb9e045F4017BfA5d26A73A0F0601Fd2"
+    const ContractTestPay = new web3.eth.Contract(TestPayABI, TestPayaddress);
+    var result = await ContractTestPay.methods.ContractBalance().call();
+    console.log(`ContractBalance shows ${Web3.utils.fromWei(result)} ether`);
+    var x = Web3.utils.fromWei(await web3.eth.getBalance(TestPayaddress), 'ether');
+    console.log(`TestPayaddress ${TestPayaddress} has ${x} ether`);
+}
+f();
