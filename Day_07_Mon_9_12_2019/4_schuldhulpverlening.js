61489452e4a88235317c4ba1686d9751f9a2f463
emroelofsen <47346232+emroelofsen@users.noreply.github.com>
2020-01-30 11:08:56 +0100

    order scheppen
    order scheppen

--- Day_07_Mon_9_12_2019/4_schuldhulpverlening.js
+++
@@ -1,5 +0,0 @@
-var schuldhulpverlening = artifacts.require("schuldhulpverlening");
-
-module.exports = function(deployer) {
-  deployer.deploy(schuldhulpverlening);
-};
---
+++ Day_07_Mon_9_12_2019/Unsorted/4_schuldhulpverlening.js
@@ -0,0 +1,5 @@
+var schuldhulpverlening = artifacts.require("schuldhulpverlening");
+
+module.exports = function(deployer) {
+  deployer.deploy(schuldhulpverlening);
+};
