83ee2662b4cf45901623e9b5bb0aecf2507d8593
emroelofsen <47346232+emroelofsen@users.noreply.github.com>
2020-01-30 14:10:07 +0100

    upgradeable
    undeclared syntax?

--- Day_08_Wed_11_12_2019/schuldhulpverlening_lozefucties.sol
+++ Day_08_Wed_11_12_2019/schuldhulpverlening_lozefucties.sol
@@ -1,18 +1,32 @@
 pragma solidity ^0.5.2; //uses the latest stable version of solidity.
-
+import "github.com/provable-things/ethereum-api/provableAPI.sol";
+//import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 /* 
 Project: Smartcontract Gemeente Den Haag
 Owner: E.M. Roelofsen (Externe HHS Student)
 Status: Alpha (no security checks or whatsoever)
 */
 
-contract schuldHulpVerlening {
+contract schuldhulpverlening is usingProvable {
     address public schuldHulpVerlener; /* This is the way in which we initialize the variable in our contract called schuldHulpVerlener, that holds a value of type address, and it has a public access property, which means an api, or a WebApp can read it. */
     address[] public debiteur; /* This is a variable named debiteur, that references a dynamic array of addresses, which also has public access, so other smart contracts or apis can read the participants in this array. */
     address[] public derdePartij; //this function isn't used for now; will be developed in Beta version.
-    
+
     bool geschiktVoorSchuldHulpVerlening;
     bool krijgtSchuldHulpVerlening;
+    
+    uint enabledAt = now;
+    modifier enabledEvery(uint t) {
+    if (now >= enabledAt) {
+      enabledAt = now + t;
+      _;      
+    }
+    }
+    
+    function __callback(bytes32 myid, string memory result ) public {
+        require(msg.sender == oraclize_cbAddress());
+        name = result;
+    } 
     
     modifier schuldHulpVerlenerOnly() {
     require(msg.sender == schuldHulpVerlener);
@@ -24,15 +38,15 @@ contract schuldHulpVerlening {
     
     function boolGeschikt() public payable schuldHulpVerlenerOnly{
         if(msg.value == 1){
-        geschiktVoorSchuldHulpVerlening == true; } else geschiktVoorSchuldHulpVerlening == false;
+        geschiktVoorSchuldHulpVerlening = true; } else geschiktVoorSchuldHulpVerlening = false;
     } /*This function allows the municipality to change the value of booleans by paying ether*/
     
     function boolKrijgt() public payable schuldHulpVerlenerOnly {
-        require(geschiktVoorSchuldHulpVerlening == true);
+        require(geschiktVoorSchuldHulpVerlening = true);
         if(msg.value == 1){
-        krijgtSchuldHulpVerlening == true; } else krijgtSchuldHulpVerlening == false;
+        krijgtSchuldHulpVerlening = true; } else krijgtSchuldHulpVerlening = false;
     } /*This function allows the municipality to change the value of booleans by paying ether*/
-   
+    
     
     
     function participantInSchuldHulpVerlening() public payable { /*The function is defined with 2 properties: a) public, which makes it publicly accessible. b) payable, which allows the function to be triggered by a transaction that sends ether along with it. The ether will be used to fund the smartcontract, and finally, reward the miners on the blockchain.*/
@@ -46,7 +60,21 @@ contract schuldHulpVerlening {
         require(msg.value > 0.1 ether); /*The second line uses the built-in function require, which asserts a condition to be true to continue the execution, or else, escape the function. In this case, we want to make sure that whoever triggers his function, transfer along with it more than 0.1 ether, as a condition to decourage scammers. */
         derdePartij.push(msg.sender); /*The third line appends the address of the account that triggered the function to the array of 3ePartij we initialized at the beginning of the contract. */
     }    
-    
+
+    function emptyArrays() public schuldHulpVerlenerOnly {
+        debiteur = new address[](0);
+        derdePartij = new address[](0);
+    } /* after the debiteur is without debt the municipality can clear the records on the blockchain*/
+  
+      function getDateTime() public payable {
+       priceOfUrl = provable_getPrice("URL");
+       require (address(this).balance >= priceOfUrl,
+            "please add some ETH to cover for the query fee");
+       provable_query("URL", 
+            "json(http://worldtimeapi.org/api/timezone/Europe/Amsterdam).datetime");
+   }
+   
+       
     /* BROKEN SYNTAX !!
     function getDebiteur() public schuldHulpVerlenerOnly view returns( address[]) {
         return debiteur;
@@ -54,28 +82,6 @@ contract schuldHulpVerlening {
     function getDerdePartij() public view returns( address[]) {
         return derdePartij;
     } *//*These two function let the municipality see who's in the arrays*/
-    
-    
-    function cleaningGDPR() public schuldHulpVerlenerOnly {
-        debiteur = new address[](0);
-        derdePartij = new address[](0);
-    } /* after the debiteur is without debt the municipality can clear the records on the blockchain*/
-    
-        string public name="Mortal";
-
-    function destroy() public  { // note add security
-        selfdestruct(msg.sender);
-    }
-
-    function destroyAndSend(address payable recipient) public {
-      // note add security
-    selfdestruct(recipient);
-    }
-    
-      string public MyName="Test publication to Etherscan";
-  function SetMyName(string calldata _MyName) external {
-     MyName = _MyName;
-  }
 }
 
 
